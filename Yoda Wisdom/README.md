1d1db242955fa275190aa8fda007d57c9fd6e86b
Nuno Macedo <nunomacedo@Nunos-MacBook-Air.local>
2019-11-16 16:52:26 -0500

    all scss files fixed and main page working

---
+++ Yoda Wisdom/README.md
@@ -0,0 +1,49 @@
+// Scope Code
+
+// - My ideia is to have a one page app where the theme is from Star Wars and the user will be able to get some Jedi Wisdom from Master Yoda.
+
+// - Once the page loads they will be prompt to answer two questions: the 1st one would be: "How are you feeling today?" The user would have radion buttons to sleect which one they would like and the otions are, option1 = Rebel / option2 = Dark. The 2nd question would be: "Which lighsaber would you choose?" option1 = Blue / option2 = Red. I would have an image or a gif with the lightsabers beside the radio button. 
+
+// - To randomize the selection of which quote will be displayed I will create two objects arrays, one for Jedi with two key values Red and Blue and inside this values I will have quotes based on if they select Rebel, Red something related to the dark side, if they select Rebel and Blue some good advise from Yoda. I will do the same for the Vader with two key values Red and Blue.
+
+// - Depending on their selection I would have a random quote from Yoda displaying below the two questions and beside Yoda's photo. 
+
+// Strech Goal
+
+// - Increase number of quotes
+// - Have the quote shown in a speaking balloon 
+// - Animate the header and have Yoda with some sort of animation too 
+
+
+
+Project 3 - Requirements:
+
+- [] It is clear to the user what the app does
+- [] Adds to the DOM dynamically using JavaScript
+- [] Interacts with the DOM (HTML) using events or taking input from the user
+- [] Modern ES6 variable declarations used (let, const)
+- [] GitHub repo has more than one significant commit
+- [] App and interactions are accessible
+- [] Site is live on GH Pages or student's own URL
+- [] Errors are handled effectively
+- [] Sass is used
+- [] Files, folders & naming conventions followed (camel case)(this includes your project repo)
+
+
+You will also be given a general mark on how well you adhere to best practices mentioned in class. Best practices include but may not be limited to:
+
+- [] Extraneous code is removed (including console.log)
+- [] Semantic HTML elements are used properly
+- [] Setup snippet is used
+- [] Wrapper used to constrain content on large displays
+- [] One external .css stylesheet is used for whole project
+- [] Site is responsive and uses media queries
+
+
+You will also be given marks for your project presentation:
+
+- [] Student was loud enough for everyone to hear
+- [] Student was able to identify a technical win
+- [] Student was able to effectively identify a technical challenge
+- [] Student did not go over time
+
--- Yoda Wisdom/script.js
+++ Yoda Wisdom/script.js
@@ -4,7 +4,7 @@
 
 // - Once the page loads they will be prompt to answer two questions: the 1st one would be: "How are you feeling today?" The user would have radion buttons to sleect which one they would like and the otions are, option1 = Rebel / option2 = Dark. The 2nd question would be: "Which lighsaber would you choose?" option1 = Blue / option2 = Red. I would have an image or a gif with the lightsabers beside the radio button. 
 
-// - To randomize the selection of which quote will be displayed I will create two objects arrays, one for Rebel with two key values Red and Blue and inside this values I will have quotes based on if they select Rebel, Red something related to the dark side, if they select Rebel and Blue some good advise from Yoda. I will do the same for the Dark with two key values Red and Blue.
+// - To randomize the selection of which quote will be displayed I will create two objects arrays, one for Jedi with two key values Red and Blue and inside this values I will have quotes based on if they select Rebel, Red something related to the dark side, if they select Rebel and Blue some good advise from Yoda. I will do the same for the Vader with two key values Red and Blue.
 
 // - Depending on their selection I would have a random quote from Yoda displaying below the two questions and beside Yoda's photo. 
 
@@ -42,7 +42,31 @@ const rebel = {
             color: 'blue'
         },
         {
-            quote: 'PATIENCE YOU MUST HAVE my young padawan.',
+            quote: 'Clear your mind must be, if you are to find the villains behind this plot.',
+            type: 'resistance',
+            color: 'blue'
+        },
+        {
+            quote: 'To be Jedi is to face the truth, and choose. Give off light, or darkness, Padawan. Be a candle, or the night.',
+            type: 'resistance',
+            color: 'blue'
+        },
+        {
+            quote: 'At an end your rule is, and not short enough it was!',
+            color: 'blue'
+        },
+        {
+            quote: 'Control, control, you must learn control!',
+            type: 'resistance',
+            color: 'blue'
+        },
+        {
+            quote: 'Already know you that which you need.',
+            type: 'resistance',
+            color: 'blue'
+        },
+        {
+            quote: 'Patience you must have my young padawan.',
             type: 'resistance',
             color: 'red'
         },
@@ -63,6 +87,31 @@ const rebel = {
         },
         {
             quote: 'Always two there are, no more, no less. A master and an apprentice.',
+            type: 'resistance',
+            color: 'red'
+        },
+        {
+            quote: 'Strong is Vader. Mind what you have learned. Save you it can.',
+            type: 'resistance',
+            color: 'red'
+        },
+        {
+            quote: 'Faith in your new apprentice, misplaced may be. As is your faith in the dark side of the Force.',
+            type: 'resistance',
+            color: 'red'
+        },
+        {
+            quote: 'To answer power with power, the Jedi way this is not. In this war, a danger there is, of losing who we are.',
+            type: 'resistance',
+            color: 'red'
+        },
+        {
+            quote: 'The dark side clouds everything. Impossible to see the light, the future is.',
+            type: 'resistance',
+            color: 'red'
+        },
+        {
+            quote: 'Blind we are, if creation of this clone army we could not see.',
             type: 'resistance',
             color: 'red'
         },
@@ -94,6 +143,31 @@ const rebel = {
             color: 'red'
         },
         {
+            quote: 'You will find only what you bring in.',
+            type: 'dark side',
+            color: 'red'
+        },
+        {
+            quote: 'Named must your fear be before banish it you can.',
+            type: 'dark side',
+            color: 'red'
+        },
+        {
+            quote: 'The boy you trained, gone he is. Consumed by Darth Vader.',
+            type: 'dark side',
+            color: 'red'
+        },
+        {
+            quote: 'I cannot teach him. The boy has no patience.',
+            type: 'dark side',
+            color: 'red'
+        },
+        {
+            quote: 'Pain, suffering, death I feel. Something terrible has happened. Young Skywalker is in pain. Terrible pain.',
+            type: 'dark side',
+            color: 'red'
+        },
+        {
             quote: 'Fear is the path to the dark side. Fear leads to anger. Anger leads to hate. Hate leads to suffering.',
             type: 'dark side',
             color: 'blue'
@@ -118,12 +192,48 @@ const rebel = {
             type: 'dark side',
             color: 'blue'
         },
+        {
+            quote: 'Hmm. In the end, cowards are those who follow the dark side.',
+            type: 'dark side',
+            color: 'blue'
+        },
+        {
+            quote: 'Stopped they must be; on this all depends. Only a fully-trained Jedi Knight, with the Force as his ally, will conquer Vader and his Emperor.',
+            type: 'dark side',
+            color: 'blue'
+        },
+        {
+            quote: 'Decide you must, how to serve them best. If you leave now, help them you could; but you would destroy all for which they have fought, and suffered.',
+            type: 'dark side',
+            color: 'blue'
+        },
+        {
+            quote: 'Younglings, younglings gather around.',
+            type: 'dark side',
+            color: 'blue'
+        },
+        {
+            quote: 'Use your feelings, Obi-Wan, and find him you will.',
+            type: 'dark side',
+            color: 'blue'
+        },
     ]
 };
 
 
 // The function will start here
 $(function() {
+
+    const playScroll = $(window).scrollTop();
+    $('.fa-angle-down, p').click(function () {
+        $('html, body').animate({scrollTop: playScroll + 580})
+    });
+
+    const wisdomScroll = $(window).scrollTop();
+    $('.submit').click(function () {
+        $('html, body').animate({scrollTop: wisdomScroll + 970})
+        $('audio#yoda')[0].play()
+    });
 
     function randomQuote(optionArray) {
         const item = Math.floor(Math.random() * optionArray.length);
@@ -140,12 +250,14 @@ $(function() {
         const color = selection.filter((choice) => {
             if(choice.color === saberOption) {
                 return true;
-            }
+            };
         });
     
         const quoteToDisplay = randomQuote(color);
 
-        $('.quote').html(`<p class="quote">${quoteToDisplay.quote}</p>`)
+        $('.quote').html(`<p class="quote">${quoteToDisplay.quote}</p>`);
+
+        $('.quote').css('display', 'block');
 
     });  
 });
